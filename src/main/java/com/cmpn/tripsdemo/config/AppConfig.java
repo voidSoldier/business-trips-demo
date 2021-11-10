8e9b6461776fd437440c857f8a5917a5f2ad909a
Irina_Suchkova <Irina_Suchkova@epam.com>
2021-11-10 17:58:49 +0500

    test commit

---
+++ src/main/java/com/cmpn/tripsdemo/config/AppConfig.java
@@ -0,0 +1,6 @@
+package com.cmpn.tripsdemo.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class AppConfig implements WebMvcConfigurer {
+}
---
+++ src/main/java/com/cmpn/tripsdemo/config/WebSecurityConfig.java
@@ -0,0 +1,6 @@
+package com.cmpn.tripsdemo.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
