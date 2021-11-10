ba8630fce72805f452cd703d4c94b230286f4653
Irina Suchkova <irina_suchkova@epam.com>
2021-11-10 15:09:45 +0500

    Initial commit

---
+++ src/main/java/com/cmpn/tripsdemo/TripsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.cmpn.tripsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripsdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmpn/tripsdemo/TripsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpn.tripsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
